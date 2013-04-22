8454e8d2005d1c70157406f75b3e58f19e80f025
Aaron <aaron.l.france@gmail.com>
2013-04-23 00:38:28 +0200

    Added scripts directory and converted various Qtile bindings.

---
+++ .config/wingo/scripts/dzen-workspaces.py
@@ -0,0 +1,43 @@
+import sys
+import datetime
+import pywingo
+
+class Active:
+    def __init__(self, num, markup):
+        self.num = num
+        self.markup = markup
+
+markup = {
+    'current':  '^fg(#00FF00)%s^fg()',
+    'visible':  '^fg(#FF0000)%s^fg()',
+    'hidden':   '^fg(#FFFFFF)%s^fg()',
+    'empty':    '^fg(#FFFFFF)%s^fg()',
+}
+
+W = pywingo.Wingo()
+
+@W.bind('AddedWorkspace')
+@W.bind('RemovedWorkspace')
+@W.bind('ChangedWorkspace')
+@W.bind('ChangedVisibleWorkspace')
+@W.bind('ChangedWorkspaceNames')
+@W.bind('ManagedClient')
+@W.bind('UnmanagedClient')
+@W.bind('Subscribed')
+def show(_):
+    current = W.GetWorkspace()
+    visible = W.GetVisibleWorkspaceList()
+    hidden = W.GetHiddenWorkspaceList()
+    vtagged = []
+    for space in visible:
+        if space == current:
+            vtagged.append(Active(space, markup['current']))
+        else:
+            vtagged.append(Active(space, markup['visible']))
+    bar = range(1,len(hidden) + len(visible) +1)
+    for head in vtagged:
+        bar[int(head.num) - 1] = "[%s]" % (head.markup % head.num)
+    print '   '.join(map(str, bar)),'     ', datetime.datetime.now().strftime("%H:%M")
+    sys.stdout.flush()
+
+W.loop()
