b357092c6f6a406183b4135a5cc662bb18e63552
Aaron France <aaron.l.france@gmail.com>
2014-01-30 23:22:50 +0100

    Put jump-to-config into local-funs.el

--- .emacs.d/local-funs.el
+++ .emacs.d/local-funs.el
@@ -3,3 +3,16 @@
   (backward-up-list)
   (mark-sexp)
   (kill-ring-save))
+
+
+(defun align-repeat (start end regexp)
+  "Repeat alignment with respect to
+     the given regular expression."
+  (interactive "r\nsAlign regexp: ")
+  (align-regexp start end
+                (concat "\\(\\s-*\\)" regexp) 1 1 t))
+
+
+(defun jump-to-config-file ()
+  (interactive)
+  (find-file (expand-file-name "~/.emacs")))
