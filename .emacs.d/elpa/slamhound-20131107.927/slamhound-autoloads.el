df2b6ec2ccca605d8c582458b14c18451bdeb4ca
Aaron France <aaron.l.france@gmail.com>
2014-02-02 21:05:11 +0100

    Add slamhound.

---
+++ .emacs.d/elpa/slamhound-20131107.927/slamhound-autoloads.el
@@ -0,0 +1,31 @@
+;;; slamhound-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (slamhound) "slamhound" "slamhound.el" (21227 29242
+;;;;;;  988843 102000))
+;;; Generated autoloads from slamhound.el
+
+(autoload 'slamhound "slamhound" "\
+Run slamhound on the current buffer.
+
+  Requires active nrepl or slime connection.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("slamhound-pkg.el") (21227 29243 23000
+;;;;;;  670000))
+
+;;;***
+
+(provide 'slamhound-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; slamhound-autoloads.el ends here
