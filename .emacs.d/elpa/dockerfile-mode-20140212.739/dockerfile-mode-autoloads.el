c3a9924cdbce9befeaf198e23244e5cbf78e0c16
Aaron France <aaron.l.france@gmail.com>
2014-03-11 23:18:30 +0100

    Dockerfile mode.

---
+++ .emacs.d/elpa/dockerfile-mode-20140212.739/dockerfile-mode-autoloads.el
@@ -0,0 +1,30 @@
+;;; dockerfile-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (dockerfile-mode) "dockerfile-mode" "dockerfile-mode.el"
+;;;;;;  (21272 62915 305746 354000))
+;;; Generated autoloads from dockerfile-mode.el
+
+(autoload 'dockerfile-mode "dockerfile-mode" "\
+A major mode to edit Dockerfiles.
+\\{dockerfile-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("dockerfile-mode-pkg.el") (21272 62915
+;;;;;;  343637 594000))
+
+;;;***
+
+(provide 'dockerfile-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dockerfile-mode-autoloads.el ends here
