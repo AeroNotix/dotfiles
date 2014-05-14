4fc0c4af8d66ac5754885bfb44f6ec479296b48a
Aaron France <aaron.l.france@gmail.com>
2014-05-14 20:55:13 +0200

    Add split-erlang-exports.

--- .emacs.d/local-funs.el
+++ .emacs.d/local-funs.el
@@ -16,3 +16,38 @@
 (defun jump-to-config-file ()
   (interactive)
   (find-file (expand-file-name "~/.emacs")))
+
+(defun trim-string (string)
+  (replace-regexp-in-string
+   "\\`[ \t\n]*" ""
+   (replace-regexp-in-string "[ \t\n]*\\'" "" string)))
+
+(defun move-to-export ()
+  (search-backward "-export"))
+
+(defun go-to-export-end ()
+  (search-forward "])."))
+
+(defun split-erlang-exports ()
+  (interactive)
+  (save-excursion
+    (if (not (looking-at "-export"))
+        (move-to-export))
+    (forward-char (length "-export(["))
+    (push-mark)
+    (if (not (looking-at "])"))
+        (progn
+          (go-to-export-end)
+          (backward-char 3)))
+    (let* ((selection
+            (buffer-substring-no-properties (region-beginning) (region-end)))
+           (exports (split-string selection ",")))
+      (move-to-export)
+      (push-mark)
+      (go-to-export-end)
+      (kill-region (region-beginning) (region-end))
+      (dolist (e exports)
+        (let ((s (format "-export([%s]).\n" (trim-string e))))
+          (insert s))))))
+
+(global-set-key (kbd "C-x C-a s e") 'split-erlang-exports)
