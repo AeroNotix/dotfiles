29c54a4da731cd63da42ca969085c32d687dc58f
Aaron France <aaron.l.france@gmail.com>
2014-05-16 18:31:38 +0200

    Remove unused function

--- .emacs.d/local-funs.el
+++ .emacs.d/local-funs.el
@@ -5,14 +5,6 @@
   (kill-ring-save))
 
 
-(defun align-repeat (start end regexp)
-  "Repeat alignment with respect to
-     the given regular expression."
-  (interactive "r\nsAlign regexp: ")
-  (align-regexp start end
-                (concat "\\(\\s-*\\)" regexp) 1 1 t))
-
-
 (defun jump-to-config-file ()
   (interactive)
   (find-file (expand-file-name "~/.emacs")))
