8c2683389105371af1723d94c40f960a2a01d277
Aaron <aaron.l.france@gmail.com>
2014-05-25 13:52:54 +0200

    Sort keys and imports.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,11 @@
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Run(spawnPipe)
 
 
 main = do
@@ -17,16 +17,15 @@ main = do
         } `additionalKeys`
         [ (( mod1Mask .|. shiftMask, xK_i),      spawn ssh_cmd)
         , (( mod1Mask .|. shiftMask, xK_Return), spawn "termite")
+        , (( mod1Mask .|. shiftMask, xK_e),      spawn "emacsclient -c")
         , (( mod1Mask .|. shiftMask, xK_f),      spawn "chromium")
-        , (( mod1Mask .|. shiftMask, xK_m),      spawn "termite -e 'ncmpcpp'")
         , (( mod1Mask .|. shiftMask, xK_g),      spawn "gimp")
         , (( mod1Mask .|. shiftMask, xK_k),      spawn "keepassx")
-        , (( mod1Mask .|. shiftMask, xK_v),      spawn "vlc")
+        , (( mod1Mask .|. shiftMask, xK_m),      spawn "termite -e 'ncmpcpp'")
         , (( mod1Mask .|. shiftMask, xK_s),      spawn "skype")
-        , (( mod1Mask,               xK_o),      sendMessage NextLayout)
+        , (( mod1Mask .|. shiftMask, xK_v),      spawn "vlc")
         , (( mod1Mask .|. shiftMask, xK_w),      spawn "steam")
         , (( mod1Mask .|. shiftMask, xK_y),      spawn "pidgin")
-        , (( mod1Mask .|. shiftMask, xK_e),      spawn "emacsclient -c")
         , (( mod1Mask,               xK_F11),    spawn "amixer -c 0 --quiet set Master 1-")
         , (( mod1Mask,               xK_F12),    spawn "amixer -c 0 --quiet set Master 1+")
         , (( mod1Mask,               xK_F4),     spawn "ncmpcpp prev")
@@ -35,5 +34,6 @@ main = do
         , (( mod1Mask,               xK_F7),     spawn "amixer set Master unmute")
         , (( mod1Mask,               xK_F8),     spawn "ncmpcpp play")
         , (( mod1Mask,               xK_F9),     spawn "ncmpcpp next")
+        , (( mod1Mask,               xK_o),      sendMessage NextLayout)
         ] `removeKeys`
         [ (( mod1Mask, xK_space)) ]
