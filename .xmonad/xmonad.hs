e019f29a3cd428a6ebf081162da216927061ac43
Aaron France <aaron.l.france@gmail.com>
2014-05-26 23:57:56 +0200

    Use functions for common spawns.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,6 +8,12 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 
 
+amixerCmd s =
+  spawn $ "amixer -c 0 --quiet set Master " ++ s
+
+ncmpcpp s =
+  spawn $ "ncmpcpp " ++ s
+
 main = do
   let ssh_cmd = "termite -e 'ssh xeno@zerolength.com -t tmux attach-session -d -t weechat'" in
     xmonad $ defaultConfig
@@ -29,14 +35,14 @@ main = do
         , (( mod1Mask .|. shiftMask, xK_v      ), spawn "vlc")
         , (( mod1Mask .|. shiftMask, xK_w      ), spawn "steam")
         , (( mod1Mask .|. shiftMask, xK_y      ), spawn "pidgin")
-        , (( mod1Mask,               xK_F11    ), spawn "amixer -c 0 --quiet set Master 1-")
-        , (( mod1Mask,               xK_F12    ), spawn "amixer -c 0 --quiet set Master 1+")
-        , (( mod1Mask,               xK_F4     ), spawn "ncmpcpp prev")
-        , (( mod1Mask,               xK_F5     ), spawn "ncmpcpp pause")
+        , (( mod1Mask,               xK_F11    ), amixerCmd "1-")
+        , (( mod1Mask,               xK_F12    ), amixerCmd "1+")
+        , (( mod1Mask,               xK_F4     ), ncmpcpp "prev")
+        , (( mod1Mask,               xK_F5     ), ncmpcpp "pause")
         , (( mod1Mask,               xK_F6     ), spawn "amixer -c 0 --quiet set Master mute")
         , (( mod1Mask,               xK_F7     ), spawn "amixer set Master unmute")
-        , (( mod1Mask,               xK_F8     ), spawn "ncmpcpp play")
-        , (( mod1Mask,               xK_F9     ), spawn "ncmpcpp next")
+        , (( mod1Mask,               xK_F8     ), ncmpcpp "play")
+        , (( mod1Mask,               xK_F9     ), ncmpcpp "next")
         , (( mod1Mask,               xK_o      ), sendMessage NextLayout)
         , (( mod1Mask,               xK_c      ), sendMessage (IncMasterN 1))
         , (( mod1Mask,               xK_v      ), sendMessage (IncMasterN (-1)))
