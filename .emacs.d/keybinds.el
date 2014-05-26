8471dcb6ef0e60d0aed830ec2bff99030afa23f1
Aaron France <aaron.l.france@gmail.com>
2014-05-26 21:14:52 +0200

    Added align-regexp to C-x a r and then aliged-regexp'd that block!

--- .emacs.d/keybinds.el
+++ .emacs.d/keybinds.el
@@ -4,19 +4,20 @@
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-w"))
 
-(global-set-key (kbd "RET")             'newline-and-indent)
-(global-set-key (kbd "C-x g")           'magit-status)
-(global-set-key (kbd "C-x q")           'jump-to-config-file)
-(global-set-key (kbd "C-<space>")       'set-mark-command)
-(global-set-key (kbd "C-c C-x")         'query-replace)
-(global-set-key (kbd "<menu>")          'idomenu)
-(global-set-key (kbd "C-x C-w")         'cider-eval-last-sexp-and-replace)
-(global-set-key (kbd "C-{")             'flymake-display-err-menu-for-current-line)
-(global-set-key (kbd "M-x")             'smex)
-(global-set-key (kbd "<f7>")            'previous-buffer)
-(global-set-key (kbd "<f8>")            'next-buffer)
-(global-set-key (kbd "C-c g r")         'gist-region)
-(global-set-key (kbd "C-c g b")         'gist-buffer)
+(global-set-key (kbd "RET")       'newline-and-indent)
+(global-set-key (kbd "C-x g")     'magit-status)
+(global-set-key (kbd "C-x q")     'jump-to-config-file)
+(global-set-key (kbd "C-<space>") 'set-mark-command)
+(global-set-key (kbd "C-c C-x")   'query-replace)
+(global-set-key (kbd "<menu>")    'idomenu)
+(global-set-key (kbd "C-x C-w")   'cider-eval-last-sexp-and-replace)
+(global-set-key (kbd "C-{")       'flymake-display-err-menu-for-current-line)
+(global-set-key (kbd "M-x")       'smex)
+(global-set-key (kbd "<f7>")      'previous-buffer)
+(global-set-key (kbd "<f8>")      'next-buffer)
+(global-set-key (kbd "C-c g r")   'gist-region)
+(global-set-key (kbd "C-c g b")   'gist-buffer)
+(global-set-key (kbd "C-x a r")   'align-regexp)
 
 ;; Transpose stuff with M-t
 (global-unset-key (kbd "M-t")) ;; which used to be transpose-words
@@ -38,5 +39,4 @@
 
 
 ;; ibuffer instead of list-buffers
-
 (global-set-key (kbd "C-x C-b") 'ibuffer)
