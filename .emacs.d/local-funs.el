4c2c4251744f2b331decc9dcd69936885c5a823c
Aaron France <aaron.l.france@gmail.com>
2014-07-16 22:45:48 +0200

    Add move-region{-up,-down} functionality

--- .emacs.d/local-funs.el
+++ .emacs.d/local-funs.el
@@ -124,6 +124,28 @@
 
 (add-hook 'erlang-mode-hook 'erlang--insert-module)
 
+(defun move-region (start end n)
+  "Move the current region up or down by N lines."
+  (interactive "r\np")
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (let ((start (point)))
+      (insert line-text)
+      (setq deactivate-mark nil)
+      (set-mark start))))
+
+(defun move-region-down (start end n)
+  "Move the current line down by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) 1 n)))
+
+(defun move-region-up (start end n)
+  "Move the current line up by N lines."
+  (interactive "r\np")
+  (move-region start end (if (null n) -1 (- n))))
+
+(global-set-key (kbd "M-<up>") 'move-region-up)
+(global-set-key (kbd "M-<down>") 'move-region-down)
 (global-set-key (kbd "C-x C-a s c") 'strip-comments)
 (global-set-key (kbd "M-n")         'copy-line-above)
 (global-set-key (kbd "C-x C-a s e") 'split-erlang-exports)
