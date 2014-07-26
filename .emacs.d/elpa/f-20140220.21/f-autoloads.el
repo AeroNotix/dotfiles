44eb0ba78cad0c48ebd78e0ecdf3d5735b084cf4
Aaron France <aaron.l.france@gmail.com>
2014-07-26 17:19:25 +0200

    Add more add-ons

---
+++ .emacs.d/elpa/f-20140220.21/f-autoloads.el
@@ -0,0 +1,18 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21459 50010 448433
+;;;;;;  415000))
+
+;;;***
+
+(provide 'f-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; f-autoloads.el ends here
