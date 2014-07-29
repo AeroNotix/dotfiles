8fbe64424fcf63da248f0b6a6746b25bb1bba024
Aaron France <aaron.l.france@gmail.com>
2014-07-30 00:19:38 +0200

    Add refacterl

---
+++ .emacs.d/elpa/refacterl-0.0.1/refacterl-autoloads.el
@@ -0,0 +1,18 @@
+;;; refacterl-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("refacterl-pkg.el" "refacterl.el") (21464
+;;;;;;  7137 731581 485000))
+
+;;;***
+
+(provide 'refacterl-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; refacterl-autoloads.el ends here
