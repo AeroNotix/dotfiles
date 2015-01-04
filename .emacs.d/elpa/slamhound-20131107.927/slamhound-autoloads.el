d70e9057ea90f02ed4dfac1a3e78106c07729979
Aaron France <aaron.l.france@gmail.com>
2015-01-04 22:03:27 +0100

    Remove unused old modes

--- .emacs.d/elpa/slamhound-20131107.927/slamhound-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; slamhound-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (slamhound) "slamhound" "../../../../.emacs.d/elpa/slamhound-20131107.927/slamhound.el"
-;;;;;;  "ed013c007a8c89a5a22026e042a2ac0a")
-;;; Generated autoloads from ../../../../.emacs.d/elpa/slamhound-20131107.927/slamhound.el
-
-(autoload 'slamhound "slamhound" "\
-Run slamhound on the current buffer.
-
-  Requires active nrepl or slime connection.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../.emacs.d/elpa/slamhound-20131107.927/slamhound-pkg.el"
-;;;;;;  "../../../../.emacs.d/elpa/slamhound-20131107.927/slamhound.el")
-;;;;;;  (21239 65214 781888 580000))
-
-;;;***
-
-(provide 'slamhound-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; slamhound-autoloads.el ends here
--- .emacs.d/elpa/smex-20140131.1006/smex-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; smex-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (smex-initialize smex) "smex" "smex.el" (21239
-;;;;;;  63590 984444 837000))
-;;; Generated autoloads from smex.el
-
-(autoload 'smex "smex" "\
-
-
-\(fn)" t nil)
-
-(autoload 'smex-initialize "smex" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("smex-pkg.el") (21239 63591 10068 507000))
-
-;;;***
-
-(provide 'smex-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; smex-autoloads.el ends here
