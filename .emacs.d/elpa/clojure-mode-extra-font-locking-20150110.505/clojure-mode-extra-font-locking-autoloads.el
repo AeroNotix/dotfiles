ec01073bcd9ae91acd87f598ba94bfcfdcf55a68
Aaron France <aaron.l.france@gmail.com>
2016-02-26 17:35:54 +0100

    Update emacs

--- .emacs.d/elpa/clojure-mode-extra-font-locking-20150110.505/clojure-mode-extra-font-locking-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; clojure-mode-extra-font-locking-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("clojure-mode-extra-font-locking.el")
-;;;;;;  (21971 43634 994965 770000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; clojure-mode-extra-font-locking-autoloads.el ends here
