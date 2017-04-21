080a52058384f1371338a516f423c983efb17349
Aaron France <aaron.l.france@gmail.com>
2017-04-21 13:25:17 +0200

    Update emacs plugins

--- .emacs.d/elpa/d-mode-20161009.1912/d-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "d-mode" "20161009.1912" "D Programming Language major mode for (X)Emacs" 'nil :keywords '("d" "programming" "language" "emacs" "cc-mode"))
