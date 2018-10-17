57f60fe0275888c374811a9ee9f2fb23ef007b82
Aaron France <aaron.l.france@gmail.com>
2018-10-17 15:34:56 +0200

    Slight custom.el tweaks

--- .emacs.d/slime.el
+++ .emacs.d/slime.el
@@ -2,3 +2,6 @@
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 (eval-after-load "auto-complete"
   '(add-to-list 'ac-modes 'slime-repl-mode))
+
+(load "~/.quicklisp/log4slime-setup.el")
+(global-log4slime-mode 1)
