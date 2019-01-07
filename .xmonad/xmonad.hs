51cc299276ef7ff59d05c87a3e672c9dd6ae2102
Aaron France <aaron.l.france@gmail.com>
2019-01-07 15:20:11 +0100

    Switch to kitty

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -21,8 +21,8 @@ main = do
         , handleEventHook = mconcat [ docksEventHook , handleEventHook defaultConfig ]
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         } `additionalKeys`
-        [ (( mod1Mask .|. shiftMask, xK_i      ), spawn "roxterm -e 'weechat'")
-        , (( mod1Mask .|. shiftMask, xK_Return ), spawn "roxterm")
+        [ (( mod1Mask .|. shiftMask, xK_i      ), spawn "kitty -e 'weechat'")
+        , (( mod1Mask .|. shiftMask, xK_Return ), spawn "kitty")
         , (( mod1Mask .|. shiftMask, xK_e      ), spawn "emacsclient -c")
         , (( mod1Mask .|. shiftMask, xK_f      ), spawn "chromium  --profile-directory=\"Default\"")
         , (( mod1Mask .|. shiftMask, xK_d      ), spawn "chromium --profile-directory=\"Profile 1\"")
