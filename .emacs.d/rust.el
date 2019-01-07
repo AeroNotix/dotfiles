0f5c2d8ed4adab463031d71caf0a34790d400636
Aaron France <aaron.l.france@gmail.com>
2019-01-07 15:20:03 +0100

    Update rust racer/ac modes

--- .emacs.d/rust.el
+++ .emacs.d/rust.el
@@ -1,7 +1,3 @@
 (require 'flymake-rust)
 (add-hook 'rust-mode-hook 'flymake-rust-load)
-
-;; TODO: Don't use hardcoded paths
-(setq racer-rust-src-path "/home/xeno/dev/rust/src/")
-(setq racer-cmd "/home/xeno/dev/racer/target/release/racer")
 (add-to-list 'load-path "/home/xeno/dev/racer/editors")
