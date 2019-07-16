db7e853910a7bba120aaac25dec0ea01679e3801
Aaron France <aaron.l.france@gmail.com>
2019-07-16 20:12:46 +0200

    ASM shits

--- .emacs.d/asm.el
+++ .emacs.d/asm.el
@@ -1,3 +1,8 @@
 ;;; ASM mode has an incredibly stupid set of ideas arround indention,
 ;;; lets just deal with indentation ourselves?
-(define-key asm-mode-map (kbd "<return>") 'newline)
+
+
+(add-hook 'asm-mode-hook
+          (lambda ()
+            (electric-indent-local-mode -1)))
+(define-key asm-mode-map (kbd "RET") 'newline)
