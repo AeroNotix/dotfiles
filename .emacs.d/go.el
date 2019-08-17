1677d2f08f8f5dffcc1263cc635c870c6acd82d8
Aaron France <aaron.l.france@gmail.com>
2019-08-17 14:51:09 +0200

    Update go mode and tweak my config for go-mode

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,14 +1,11 @@
-(add-to-list 'load-path "~/dev/go-mode.el/" t)
-(autoload 'gofmt-before-save "go-mode" t nil)
-(add-hook 'before-save-hook #'gofmt-before-save)
 (require 'go-mode)
+(require 'go-autocomplete)
 
 (add-hook 'go-mode-hook
           '(lambda ()
              (global-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-
 (add-hook 'go-mode-hook
           '(lambda ()
              (global-set-key (kbd "C-c C-a") 'go-import-add)))
-
-(require 'go-autocomplete)
+(add-hook 'completion-at-point-functions 'go-complete-at-point)
+(add-hook 'before-save-hook #'gofmt-before-save)
