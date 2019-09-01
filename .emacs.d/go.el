c04501abf3df019b4e7917a73acb19a6e43e8944
Aaron France <aaron.l.france@gmail.com>
2019-09-01 22:56:16 +0200

    Speed up go import add

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -3,6 +3,22 @@
 (require 'company)
 (require 'company-go)
 
+(defun go-packages-find ()
+  (sort
+   (delete-dups
+    (cl-mapcan (lambda (topdir)
+                 (let ((pkgdir (concat topdir "/pkg")))
+                   (mapcar (lambda (file)
+                             (let ((sub (substring file 0 -2)))
+                               (mapconcat #'identity (cdr (split-string sub "/")) "/")))
+                           (split-string (shell-command-to-string
+                                          (format "find \"%s\" -not -path \"%s/tool*\" -not -path \"%s/obj/*\" -name \"*.a\"  -printf \"%%P\\n\""
+                                                  pkgdir pkgdir pkgdir))))))
+               (go-root-and-paths)))
+   #'string<))
+
+(setq go-packages-function 'go-packages-find)
+
 (add-hook 'go-mode-hook
           '(lambda ()
              (global-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
