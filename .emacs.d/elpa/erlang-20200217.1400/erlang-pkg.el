bb5fe6ef1c01e1396528966483bdede976a8a9ac
Aaron France <aaron.l.france@gmail.com>
2020-02-18 20:49:24 +0100

    update emacs packages

---
+++ .emacs.d/elpa/erlang-20200217.1400/erlang-pkg.el
@@ -0,0 +1,5 @@
+(define-package "erlang" "20200217.1400" "Erlang major mode"
+  '((emacs "24.1")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/golint-20180221.2015/golint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "golint" "20180221.2015" "lint for the Go source code" 'nil :commit "910be7a94367618fd0fd25eaabbee4fdc0ac7092" :url "https://github.com/golang/lint")
