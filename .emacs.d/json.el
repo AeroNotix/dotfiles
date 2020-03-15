f8afd61958237183b93a1c742f29439128682242
Aaron France <aaron.l.france@gmail.com>
2020-03-15 18:51:57 +0100

    json mode tabs

--- .emacs.d/json.el
+++ .emacs.d/json.el
@@ -1,4 +1,7 @@
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+
 (add-hook 'json-mode-hook
           (lambda ()
+            (make-local-variable 'indent-tabs-mode)
             (make-local-variable 'js-indent-level)
-            (setq js-indent-level 2)))
+            (setq indent-tabs-mode t)))
