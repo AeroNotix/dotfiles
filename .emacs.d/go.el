064591aab7a0877d2bac35cec6f2f7c020214b76
Aaron France <aaron.l.france@gmail.com>
2020-03-15 18:51:49 +0100

    use go lsp

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,39 +1,4 @@
-(require 'go-mode)
-(require 'go-autocomplete)
-(require 'company)
-(require 'company-go)
-
-(defun go-packages-find ()
-  (sort
-   (delete-dups
-    (cl-mapcan (lambda (topdir)
-                 (let ((pkgdir (concat topdir "/pkg")))
-                   (mapcar (lambda (file)
-                             (let ((sub (substring file 0 -2)))
-                               (mapconcat #'identity (cdr (split-string sub "/")) "/")))
-                           (split-string (shell-command-to-string
-                                          (format "find \"%s\" -not -path \"%s/tool*\" -not -path \"%s/obj/*\" -name \"*.a\"  -printf \"%%P\\n\""
-                                                  pkgdir pkgdir pkgdir))))))
-               (go-root-and-paths)))
-   #'string<))
-
-(setq go-packages-function 'go-packages-find)
-
-(add-hook 'go-mode-hook
-          '(lambda ()
-             (global-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-(add-hook 'go-mode-hook
-          '(lambda ()
-             (global-set-key (kbd "C-c C-a") 'go-import-add)))
-(add-hook 'go-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "M-.") 'godef-jump)))
-(add-hook 'before-save-hook #'gofmt-before-save)
-(add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (flycheck-mode)
-                          (company-mode)))
-
 (add-hook 'go-mode-hook
           #'(lambda ()
               (setq gofmt-command "goimports")))
+(add-hook 'go-mode-hook #'lsp-deferred)
