1f368a6796f38416262ba13251e58a41b480ac1a
Aaron France <aaron.l.france@gmail.com>
2020-03-16 00:15:56 +0100

    fix: switch back to tide

--- .emacs.d/typescript.el
+++ .emacs.d/typescript.el
@@ -13,4 +13,21 @@ cons cell (regexp . minor-mode)."
           (lambda ()
             (make-local-variable 'tab-width)
             (setq-local tab-width 2)))
-(add-hook 'typescript-mode-hook #'lsp-deferred)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically
+        '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(add-hook 'tide-mode-hook
+          (lambda ()
+            (local-set-key (kbd "M-.") 'tide-jump-to-definition)))
