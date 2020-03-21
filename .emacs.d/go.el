e6acf73ad662d14f2e60e9b9510497400500344c
Aaron France <aaron.l.france@gmail.com>
2020-03-21 13:39:47 +0100

    gofmt-before-save

--- .emacs.d/go.el
+++ .emacs.d/go.el
@@ -1,4 +1,6 @@
 (add-hook 'go-mode-hook
           #'(lambda ()
               (setq gofmt-command "goimports")))
+
+(add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook #'lsp-deferred)
