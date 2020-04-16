35455427985acece08ab7eb900b4769b7009a3b0
Aaron France <aaron.l.france@gmail.com>
2020-04-17 01:47:59 +0200

    fix: USE ALACRITTY UP IN THIS

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -23,7 +23,7 @@ main = do
         , handleEventHook = mconcat [ docksEventHook , handleEventHook defaultConfig <+> fullscreenEventHook]
         , layoutHook = avoidStruts $ layoutHook defaultConfig
         } `additionalKeys`
-        [ (( mod1Mask .|. shiftMask, xK_Return ), spawn "urxvt")
+        [ (( mod1Mask .|. shiftMask, xK_Return ), spawn "alacritty")
         , (( mod1Mask .|. shiftMask, xK_e      ), spawn "emacsclient -c")
         , (( mod1Mask .|. shiftMask, xK_f      ), spawn "chromium --profile-directory=\"Default\"")
         , (( mod1Mask .|. shiftMask, xK_d      ), spawn "chromium --profile-directory=\"Profile 1\"")
