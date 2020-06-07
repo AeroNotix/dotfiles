0dd8b18640019c8c0e50a07b46c900e4596815b3
Aaron France <aaron.l.france@gmail.com>
2020-06-07 22:42:50 +0200

    fix: comment out slime shit for now

--- .emacs.d/slime.el
+++ .emacs.d/slime.el
@@ -1,21 +1,19 @@
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
-(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'slime-repl-mode))
+;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
+;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+;; (eval-after-load "auto-complete"
+;;   '(add-to-list 'ac-modes 'slime-repl-mode))
 
-(defun load-if-exists-p (path)
-  (let ((e-path (expand-file-name path)))
-    (when (file-exists-p e-path)
-      (load e-path))))
+;; (defun load-if-exists-p (path)
+;;   (let ((e-path (expand-file-name path)))
+;;     (when (file-exists-p e-path)
+;;       (load e-path))))
 
-;; Slime
-(load-if-exists-p "~/.quicklisp/slime-helper.el")
-(load-if-exists-p "~/.quicklisp/log4slime-setup.el")
+;; ;; Slime
+;; (load-if-exists-p "~/.quicklisp/slime-helper.el")
+;; (load-if-exists-p "~/.quicklisp/log4slime-setup.el")
 
-(setq slime-lisp-implementations
-      '((sbcl ("sbcl"))))
+;; (setq slime-lisp-implementations
+;;       '((sbcl ("sbcl"))))
 
-(require 'slime)
-(slime-setup '(slime-fancy ac-slime))
-
-
+;; (require 'slime)
+;; (slime-setup '(slime-fancy ac-slime))
