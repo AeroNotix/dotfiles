6282bfa1f53b28b43d0d24dccabd8f88ecf971af
Aaron France <aaron.l.france@gmail.com>
2020-06-09 12:59:48 +0200

    fix: update hist

--- .emacs.d/transient/history.el
+++ .emacs.d/transient/history.el
@@ -1,9 +1,15 @@
-((magit-commit nil)
+((magit-blame
+  ("-w"))
+ (magit-commit nil)
+ (magit-dispatch nil)
+ (magit-fetch nil)
  (magit-gitignore nil)
  (magit-log
   ("-n256" "--graph" "--decorate"))
+ (magit-merge nil)
  (magit-pull nil)
  (magit-push nil
              ("--force-with-lease"))
  (magit-rebase nil)
- (magit-stash nil))
+ (magit-stash nil)
+ (magit-submodule nil))
