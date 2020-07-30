b7e913b974162fd3dd91cac7c6422ea8a38b8305
Aaron France <aaron.l.france@gmail.com>
2020-07-30 02:33:10 +0200

    some useful funcs for terraform

--- .emacs.d/terraform.el
+++ .emacs.d/terraform.el
@@ -2,3 +2,17 @@
  '(terraform-indent-level 2))
 
 (add-hook 'terraform-mode-hook #'lsp-deferred)
+
+(defun insert-version-label ()
+  (interactive)
+  (insert (format "version = \"${local.%s_version}\"" (file-name-sans-extension (file-name-nondirectory (buffer-file-name))))))
+
+(defun insert-squad-owner ()
+  (interactive)
+  (end-of-buffer)
+  (previous-line)
+  (newline-and-indent)
+  (previous-line)
+  (insert "squad_owner = \"customer\"")
+  (terraform-format-buffer)
+  (save-buffer))
