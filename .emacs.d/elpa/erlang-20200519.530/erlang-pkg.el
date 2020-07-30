760e522da3093bb6997652952c8f4d9ce7fe2dee
Aaron France <aaron.l.france@gmail.com>
2020-07-30 02:32:39 +0200

    update all the emacs

---
+++ .emacs.d/elpa/erlang-20200519.530/erlang-pkg.el
@@ -0,0 +1,6 @@
+(define-package "erlang" "20200519.530" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "c4d07f8f26565fb4360fb1f2c9cde97b8bfbd547")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/notmuch-20200722.2248/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20200722.2248" "run notmuch within emacs" 'nil :commit "c477d7ce311335fda16a15e624ca3931d79144cf" :url "https://notmuchmail.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
