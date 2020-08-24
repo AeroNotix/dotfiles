aad1701e4246de0575ed218a7683aff02cf44637
Aaron France <aaron.l.france@gmail.com>
2020-08-24 02:15:42 +0200

    update all the things

---
+++ .emacs.d/elpa/notmuch-20200822.1223/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20200822.1223" "run notmuch within emacs" 'nil :commit "68bff37dd75eacdc6c041a7d40260d2be1289571" :url "https://notmuchmail.org")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
