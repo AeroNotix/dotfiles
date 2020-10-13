c7ba0aa8f338beff3fadf5691a76b5b559de58ae
Aaron France <aaron.l.france@gmail.com>
2020-10-14 01:10:09 +0200

    fix: update emacs

---
+++ .emacs.d/elpa/erlang-20200902.641/erlang-pkg.el
@@ -0,0 +1,6 @@
+(define-package "erlang" "20200902.641" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "66b28bdd51899b60e0bb44d46ae4d0ae6b89601f")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
