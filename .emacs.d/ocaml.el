c5ca997bef081cfd37e5b392ae0bc14d9e2a32a3
Aaron France <aaron.l.france@gmail.com>
2021-06-21 12:36:26 +0200

    enable taureg/etc for opam in emacs

--- .emacs.d/ocaml.el
+++ .emacs.d/ocaml.el
@@ -1,4 +1,9 @@
-(push "/usr/share/emacs/site-lisp" load-path)
+
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
+(setq merlin-command "ocamlmerlin")  ; needed only if ocamlmerlin not already in your PATH
+(add-hook 'caml-mode-hook #'merlin-mode)
+(add-hook 'tuareg-mode-hook #'merlin-mode)
 (autoload 'merlin-mode "merlin" "Merlin mode" t)
-(add-hook 'tuareg-mode-hook 'merlin-mode)
 (load-file "~/.emacs.d/ocp-indent.el")
